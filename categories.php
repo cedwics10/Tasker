6bfbb8f3bc78c1cb48dd842414cc5307569c2234
cedrvn10 <brochettes40@outlook.fr>
2022-09-04 15:59:14 +0200

    New function to show categories of tasks

--- categories.php
+++ categories.php
@@ -1,25 +1,36 @@
 <?php 
 require_once('includes/head.php');
 
-$stmt = $pdo->query("SELECT * FROM categories");
-$result_exists = false;
-?>
-
-- Liste des catégories déjà créées :</br>
-<?php
-while($row = $stmt->fetch())
+function show_categories($pdo)
 {
-    echo $row['categorie']."<br />\n";
+	$result_exists = false;
+	$stmt = $pdo->query("SELECT * FROM categories");
+	while($row = $stmt->fetch())
+	{
+		echo "* " . $row['categorie'] . "<br />\n";
+		if(!$result_exists)
+		{
+			$result_exists = true;
+		}
+	}
+	
 	if(!$result_exists)
 	{
-		$catagorie_exists = true;
+		echo 'Aucune catégorie n\'a été créée<br />';
 	}
 }
-
-if(!$result_exists)
-{
-	echo 'Aucune catégorie n\'a été créée<br />';
-}
-?><br />
-Revenir à l'accueil : <a href="index.php">cliquez-ici</a>
+?>
+Gérez vos catégories créées.</br></br>
+- Liste des catégories déjà créées :</br>
+<?php
+show_categories($pdo)
+?><hr />
+Créer une nouvelle catégorie :
+<form action="categories.php" method="post">
+<input type="text" name="categorie"/> <input type="submit">
+</form>
 <hr /></br>
+Revenir à l'accueil : <a href="index.php" label="Retour à l'accueil">cliquez-ici</a>
+<?php 
+require_once('includes/footer.php');
+?>
