fe6a26356bf46499c524938edeb80afac33e87e5
cedrvn10 <brochettes40@outlook.fr>
2022-09-07 20:36:12 +0200

    Coding the tasks form, next step

--- includes/include_taches.php
+++ includes/include_taches.php
@@ -6,7 +6,7 @@ function options_categories($pdo)
 	$texte_options = '';
 	while($row = $stmt->fetch())
 	{
-		$texte_options = $texte_options . ' <option value="' . $row['id'] . '">' . $row['categorie'] . '</option>';
+		$texte_options = $texte_options . ' <option value="' . intval($row['id']) . '">' . $row['categorie'] . '</option>';
 	}
 	return $texte_options;
 }
@@ -17,38 +17,46 @@ if(isset($_POST['nouvelle_tache']))
 {
 	if(isset($_POST['categorie_parcourir']) and isset($_POST['nom_tache']) and isset($_POST['date']))
 	{
-		$sql_query = 'SELECT categories.* FROM categories WHERE categories.categorie = ' . $_POST['categorie_parcourir'] . '';
+		$sql_query = 'SELECT COUNT(*) FROM categories WHERE categories.id = ' . intval($_POST['categorie_parcourir']) . '';
 		$res = $pdo->query($sql_query);
 		$nb_cat_tache = $res->fetchColumn();
+		
 		if($nb_cat_tache == 1)
 		{
-			$sql_query = 'SELECT COUNT(taches.*) FROM taches WHERE  taches.id_categorie = ' . $_POST['categorie_parcourir'] . ' AND taches.nom_tache = "' . $_POST['nom_tache'] .'"';
+			$sql_query = 'SELECT COUNT(*) FROM taches WHERE  taches.id_categorie = ' . $_POST['categorie_parcourir'] . ' AND taches.nom_tache = "' . $_POST['nom_tache'] .'"';
 			$res = $pdo->query($sql_query);
 			$nb_taches_idtq = $res->fetchColumn();
-			if($nb_taches_idtq == 1)
+
+			if($nb_taches_idtq == 0)
 			{
-				if(preg_match("#^[0-9]{}-[0-9]{2}-[0-9]{4}$#", $_POST['date']))
+				if(preg_match("#^[0-9]{4}-[0-9]{2}-[0-9]{2}$#", $_POST['date']))
 				{
-					print("date ok categorie ok titre ok");
+					/*
+					$sql = "INSERT INTO taches (categorie) VALUES (?)";
+					$stmt= $pdo->prepare($sql);
+					$stmt->execute([$categorie]);
+					*/
+					echo('Nouvelle tâche envoyée avec succès');
 				}
 				else
 				{
-					print("La date entrée est incorrecte.<br />");
+					echo('La date entrée est dans un format incorrect.<br />');
 				}
 			}
 			else
 			{
-				print("Une tâche a déjà un nom identique à ce que vous voulez créer dans la catégorie courante de cette tâche.");
+				echo("Une tâche a déjà un nom identique à ce que vous voulez créer dans la catégorie courante de cette tâche.");
 			}
 		}
 		else
 		{
-			print("La catégorie pour la tâche n'existe pas : $nb_taches_idtq <br />");
+			echo("La catégorie pour la tâche n'existe pas : $nb_cat_tache <br />");
+			echo("NB CAT TACHE : " . $nb_cat_tache . " , $sql_query , $nb_cat_tache ");
 		}
 	}
 	else
 	{
-		print('Vous n\'avez pas rempli le formulaire.');
+		echo('Vous n\'avez pas rempli le formulaire.');
 	}
 }
 
