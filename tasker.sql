4199b100571f676c3adbc90f68e4afbc72633ac8
cedrvn10 <brochettes40@outlook.fr>
2022-09-17 23:53:07 +0200

    Update database

--- tasker.sql
+++ tasker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 06 sep. 2022 à 22:31
+-- Généré le : sam. 17 sep. 2022 à 23:47
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 8.1.6
 
@@ -37,9 +37,9 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `categorie`) VALUES
-(7, 'chat'),
-(8, 'chien'),
-(9, 'courses');
+(10, 'Chien'),
+(12, 'Devoir'),
+(13, 'Cuisine');
 
 -- --------------------------------------------------------
 
@@ -54,6 +54,15 @@ CREATE TABLE `taches` (
   `description` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `taches`
+--
+
+INSERT INTO `taches` (`id`, `id_categorie`, `nom_tache`, `description`, `date`) VALUES
+(1, 12, 'Devoirs d\'angalis', 'Finir l\'exercice de résumer le texte sur la statue de la liberté.	', '2022-09-19 22:00:00'),
+(2, 10, 'Amener le chien chez le véto', 'Le chien se gratte souvent.	', '2022-09-16 22:00:00'),
+(3, 13, 'Faire des cookies', 'Ne pas oublier la recette du livre de cuisine ce jour-là !	', '2022-09-21 22:00:00');
 
 --
 -- Index pour les tables déchargées
@@ -80,13 +89,13 @@ ALTER TABLE `taches`
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT pour la table `taches`
 --
 ALTER TABLE `taches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Contraintes pour les tables déchargées
