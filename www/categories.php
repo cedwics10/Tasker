2500307c6f2d5002e9bdb752bca180f43c034cf3
cedwics10 <57799856+cedwics10@users.noreply.github.com>
2022-10-11 17:04:49 +0200

    few folder updates

--- www/categories.php
+++ www/categories.php
@@ -4,12 +4,9 @@ require_once('../includes/php/include_categories.php');
 require_once('../includes/html/include_head.html');
 require_once('../includes/html/include_header.html');
 ?>
-<h1 style="text-align: center;">Gérez vos catégories créées.</h1></br></br>
-Revenir à l'accueil : <a href="index.php" label="Retour à l'accueil">cliquez-ici</a></br>
-<?php echo $message_user; ?>
-<br />
-<u>Liste des catégories déjà créées :</u>
-<table>
+
+<table class="bloc_infos">
+	<caption><u>Liste des catégories déjà créées :</u></caption>
 <tr><td>ID</td><td>Titre</td><td>Nb tâches</td><td>X</td><td>E</td></tr>
 <?php
 show_categories($pdo)
