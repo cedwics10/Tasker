4707e901d73106ed71866c23ae27e2f0b0dfd169
cedrvn10 <brochettes40@outlook.fr>
2022-10-22 13:39:58 +0200

    New unit test for qmark function and check avatar

--- includes/php/tests/checkavatarTest.php
+++ includes/php/tests/checkavatarTest.php
@@ -3,5 +3,14 @@ use PHPUnit\Framework\TestCase as TestCase;
 
 class checkavatarTest extends TestCase
 {
+    public function testEmptyCheckAvatar() 
+    {
+        $this->assertEquals(check_avatar(), true, 'Empty FILES = no upload test.');
+    }
+
+    public function testUploadAvatar() 
+    {
+        $this->assertEquals(check_avatar(), true, 'Empty FILES = no upload test.');
+    }
 }
 ?>
--- includes/php/tests/qmarkTest.php
+++ includes/php/tests/qmarkTest.php
@@ -13,7 +13,39 @@ class qmarkTest extends TestCase
         $this->assertEquals(qmark_part([], [], 'test'), '?test', 'Test qmark_mark function with no arguments except a text.');
     }
 
-    # Create tests with a _GET function
+    public function testEmptyWMockQmark()
+    {
+        $this->assertEquals(qmark_part([], [], 'test'), '?test', 'Test qmark_mark function with no arguments except a text.');
+    }
+
+    public function testOneMockGetQmark()
+    {
+        $this->assertEquals(qmark_part([], ['test' => 'ok'], ''), '?test=ok', 'Test qmark_mark function with one get mock argument.');
+    }
+
+    public function testTwoMockMinusOneGetQmark()
+    {
+        $_GET['its'] = 'notok';
+        $this->assertEquals(qmark_part(['its'], ['test' => 'ok'], ''), '?test=ok', 'Test qmark_mark function with no arguments except a text.');
+    }
+
+    public function testOneMockTwoGetQmark()
+    {
+        $_GET['its'] = 'notok';
+        $_GET['test'] = 'ok';
+        $this->assertEquals(qmark_part(['its'], ['but' => 'ilikeit'], ''), '?but=ilikeit&amp;test=ok', 'Test qmark_mark function with no arguments except a text.');
+    }
+
+    public function testTwoMockTwoGetAnchortQmark()
+    {
+        $_GET['its'] = 'notok';
+        $_GET['his'] = 'test';
+        $_GET['test'] = 'ok';
+        $_GET['to'] = 'you';
+
+        $this->assertEquals(qmark_part(['its', 'to'], ['but' => 'ilikeit'], '#index'), '?but=ilikeit&amp;his=test&amp;test=ok#index', 'Test qmark_mark function with no arguments except a text.');
+    }
+    # Create tests with injecting $_GET into the assertEqual
 }
 
 ?>
