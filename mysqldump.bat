3f5a673ee7fa8715479e1ee85b19959a816a6754
cedrvn10 <brochettes40@outlook.fr>
2022-11-08 22:33:55 +0100

    Repair mysqldump

--- mysqldump.bat
+++ mysqldump.bat
@@ -2,8 +2,8 @@
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
 
-SET backupdir=C:\Projets\Tasker
-SET user=root
+SET backupdir=C:\Projets\Tasker\sql_backups
+SET mysqluername=root
 SET database=tasker
 
-mysqldump -u%user% -p %database% > %backupdir%\sql_backups\%database%_%mydate%_%mytime%_.sql
+C:\xampp\mysql\bin\mysqldump.exe -u%mysqluername% %database% > %backupdir%\%database%_%mydate%_%mytime%.sql
--- sql_backups/tasker_-11-2022_2233.sql
+++ sql_backups/tasker_-11-2022_2233.sql
@@ -24,9 +24,10 @@ DROP TABLE IF EXISTS `categories`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_membre` int(11) NOT NULL,
   `categorie` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +36,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (30,'Maison'),(31,'Coder encore');
+INSERT INTO `categories` VALUES (43,1,'twenty campuss');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,7 +54,7 @@ CREATE TABLE `membres` (
   `photo` varchar(200) NOT NULL,
   `role` char(8) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +63,7 @@ CREATE TABLE `membres` (
 
 LOCK TABLES `membres` WRITE;
 /*!40000 ALTER TABLE `membres` DISABLE KEYS */;
-INSERT INTO `membres` VALUES (1,'jeanpierre','$2y$10$1QRCAiY3d45X9pHWFG/vi.f5D6GsL647aJlyPIOQ0BfHnILqkreni','avatars/jeanpierre.jpg','a'),(2,'yoooooooooo','$2y$10$5blvI3pk/KuNL3rYvWWFOO/.duDoaoBNRZRKs/JIeHE9YdHTBRWd6','avatars/./yoooooooooo','m'),(3,'jeanpierrejeanpierre','$2y$10$E2jgc1xDLZPMW4C.ojiv0u4vjN6OeNPu/IgNzd55fFUj6LxVDWd6G','avatars/./jeanpierrejeanpierre','m'),(4,'jeanbanane','$2y$10$EnORMqauvPF3SCmCgyCBh.aeJbXl3kRX0ecdddS0RMTgOdxlme.gC','avatars/./jeanbanane','m'),(5,'paulpierre','$2y$10$n2nvHu5BpY4sjLZYEONpvO2buwL1xkLMLFja2A7erkHT4OLWWFKXS','avatars/./paulpierre','m');
+INSERT INTO `membres` VALUES (1,'jeanpierre','$2y$10$1QRCAiY3d45X9pHWFG/vi.f5D6GsL647aJlyPIOQ0BfHnILqkreni','avatars/jeanpierre.jpg','a'),(2,'yoooooooooo','$2y$10$5blvI3pk/KuNL3rYvWWFOO/.duDoaoBNRZRKs/JIeHE9YdHTBRWd6','avatars/./yoooooooooo','m'),(3,'jeanpierrejeanpierre','$2y$10$E2jgc1xDLZPMW4C.ojiv0u4vjN6OeNPu/IgNzd55fFUj6LxVDWd6G','avatars/./jeanpierrejeanpierre','m'),(4,'jeanbanane','$2y$10$EnORMqauvPF3SCmCgyCBh.aeJbXl3kRX0ecdddS0RMTgOdxlme.gC','avatars/./jeanbanane','m'),(5,'paulpierre','$2y$10$n2nvHu5BpY4sjLZYEONpvO2buwL1xkLMLFja2A7erkHT4OLWWFKXS','avatars/./paulpierre','m'),(6,'cocococo','$2y$10$17du5ExynVCjo42UVwllRu7zYZKNx1.gqyRWlMwwc1OYQQkQXWfoG','','m');
 /*!40000 ALTER TABLE `membres` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,6 +76,7 @@ DROP TABLE IF EXISTS `taches`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `taches` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_membre` int(11) NOT NULL,
   `id_categorie` int(11) NOT NULL,
   `nom_tache` varchar(200) NOT NULL,
   `description` text NOT NULL,
@@ -85,7 +87,7 @@ CREATE TABLE `taches` (
   PRIMARY KEY (`id`),
   KEY `taches` (`id_categorie`),
   CONSTRAINT `taches` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +96,7 @@ CREATE TABLE `taches` (
 
 LOCK TABLES `taches` WRITE;
 /*!40000 ALTER TABLE `taches` DISABLE KEYS */;
-INSERT INTO `taches` VALUES (23,31,'Coder le football 2','J\'aime pas le foot mais faut que je me force hein !','2022-10-29 15:14:33','2022-10-25 22:00:00',1,1),(25,30,'BATIR EE','','2022-10-28 22:00:00','2022-10-02 22:00:00',3,1),(26,30,'CONSTRUIRE','','2022-10-29 15:14:35','2022-09-30 22:00:00',3,1),(27,30,'CONSTRUIRE 2','','2022-10-12 20:08:55','2022-10-18 22:00:00',1,0);
+INSERT INTO `taches` VALUES (40,1,43,'Manger des pâtes','efaefazea','2022-11-08 06:15:38','2022-11-03 23:00:00',3,1),(41,1,43,'La petite chambre tranquille','','2022-11-08 06:15:33','2022-11-05 23:00:00',1,0);
 /*!40000 ALTER TABLE `taches` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -107,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-02 20:03:55
+-- Dump completed on 2022-11-08 22:33:01
