5b003631fdea683e276c45ab4e37b78f9221a132
cedrvn10 <brochettes40@outlook.fr>
2022-11-14 22:41:24 +0100

    repair options on my code

--- www/inscription.php
+++ www/inscription.php
@@ -1,35 +1,6 @@
 <?php
 require_once('../includes/php/include_base.php');
 require_once('../includes/php/include_inscription.php');
-
-if(isset($_POST['btsubmit']) and isset($_SESSION['id']))
-{
-    $donnees_membre = select_member_data();
-    $member_avatar = $donnees_membre['photo'];
-    $member_hash_mdp = $donnees_membre['mdp'];
-
-    $error_message_password = error_form_password();
-    if($error_message_password === false)
-    {
-        $member_hash_mdp = password_hash($_POST['n_mot_de_passe'], PASSWORD_DEFAULT);
-        $error_message_password = 'Le mot de passe a bien été modifié !';
-    }
-
-    $error_message_avatar = error_form_avatar();
-    if($error_message_avatar  === false)
-    {
-        $member_avatar = 'avatars/./' . changebasename($_FILES['avatar']['name'], $_SESSION['pseudo']);
-
-        move_uploaded_file($_FILES['avatar']['tmp_name'], $member_avatar);
-        $error_message_avatar = 'L\'avatar a bien été modifié !';
-    }
-    
-    $error_message = $error_message_password . 
-    '<br />' . $error_message_avatar;
-
-    update_photo_avatar($member_hash_mdp,  $member_avatar);
-}
-
 require_once('../includes/html/include_head.html');
 require_once('../includes/html/include_header.html');
 ?>
